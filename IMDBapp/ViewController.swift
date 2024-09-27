3854e4c75f20d34cec6c8f93c65b5a067dc425b3
Bayram Yelec <bayrammyelec@gmail.com>
2024-09-27 14:17:42 +0300

    Initial Commit

---
+++ IMDBapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IMDBapp
+//
+//  Created by Bayram Yeleç on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
